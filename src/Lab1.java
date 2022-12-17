d2442387ba11699a0b085d489dda76b3c8fc8e7e
Rowida <roro.reffaie@gmail.com>
2022-12-17 18:06:35 +0300

    first print statement by Rowida

---
+++ src/Lab1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dr.Motaz AlReffaie
+ */
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello everyone we are group 3 ");
+        System.out.println(" i'm student number 1 and my name is Rowida Alreffaie ");
+    }
+    
+}
