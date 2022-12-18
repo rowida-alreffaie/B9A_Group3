c85e52429a2b2f8a4de720f89112c7b5760e3d69
raghad_ajlan <raghadfaisalajlan1.1@gmail.com>
2022-12-18 14:05:02 +0300

    raghad commit

--- src/Lab1.java
+++ src/Lab1.java
@@ -19,7 +19,7 @@ public class Lab1 {
         System.out.println(" i'm student number 1 and my name is Rowida Alreffaie ");
         System.out.println(" i'm student number 2 and my name is Futoon Mojalled ");
         System.out.println(" i'm student number 3 and my name is Shaimaa Alahdal and i love pizza (; ");
-        
+        System.out.println(" i'm student number 4 and my name is Raghad Ajlan... she's a cat person <3");
               
     }
     
